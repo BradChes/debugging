790f0c966738a6cc8dedf77fceb5da01da58a8c0
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2020-04-04 14:09:11 +0100

    Init project

---
+++ README.md
@@ -0,0 +1,10 @@
+# Debugging
+<p align="center">
+  <img src="demo/app-icon.png " alt="Debugging application icon"
+	  title="Debugging application icon" align="center" width="200" height="200" />
+</p>
+</br>
+Samples of debugging in Xcode.
+
+## Examples within this code
+- To be filled in.
---
+++ debugging/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  debugging
+//
+//  Created by Bradley Chesworth on 04/04/2020.
+//  Copyright © 2020 Brad Chesworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
