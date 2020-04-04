0c4f2e7b3fb09f2856022b05903f07b5b020627f
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2020-04-04 14:33:10 +0100

    Example of how to use breakpoints within xcode

--- debugging/ViewController.swift
+++ debugging/ViewController.swift
@@ -13,13 +13,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Example of using print to debug
+        // Example of using print to debug.
         print("I'm inside the viewDidLoad() method.")
         print(1, 2, 3, 4, 5, separator: "-", terminator: "")
         
-        // Example of assertions to debug
+        // Example of assertions to debug.
         assert(1 == 1, "Math failure." ) // Will Pass
         assert(1 == 2, "Math failure." ) // Will Fail
+            
+        // Example of breakpoints to debug.
+        // Selecting the line where print is will place a breakpoint (a blue marker will appear).
+        // Clicking again will disable the breakpoint, right click and select "delete breakpoint" to remove it.
+        // Breakpoints can be made conditional. Right clicking a breakpoint and selecting "edit breakpoint" opens up another window where a condition can be entered. This means the breakpoint will only be hit is that condition is true.
+        for i in 1 ... 100 {
+            print("Got number \(i).")
+        }
     }
 }
 
